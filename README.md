c46245452cd3f12c1f8684d53f8d0ab8b56c9a66
root <root@MSI.>
2025-01-17 07:58:19 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grader App
+
+Temporary solution as a workaround for the broken grade-fetcher to support the self-paced online courses.
---
+++ backend/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+const sanitize = require("sanitize-html");
+const emailValidator = require("email-validator");
+require("dotenv").config();
+
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN + "" }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/form-submit", async (req, res) => {
+  const { email } = req.body;
+
+  if (!emailValidator.validate(email)) {
+    return res.status(400).json({ error: "Invalid email address." });
+  }
+  const sanitizedEmail = sanitize(email);
+
+  const raw = JSON.stringify({
+    email: `${sanitizedEmail}`,
+  });
+
+  try {
+    const externalApiUrl = process.env.EXTERNAL_API_URL;
+    const externalApiResponse = await fetch(externalApiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: raw,
+    });
+
+    if (!externalApiResponse.ok) {
+      throw new Error(
+        `External API request failed: ${externalApiResponse.status}`
+      );
+    }
+
+    const externalApiData = await externalApiResponse.json();
+
+    res.json({
+      message: "Email submitted successfully!",
+      externalApiResponse: externalApiData,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the request." });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server Listening on port 5000");
+});
